56789e059cd49d841feb367b9cd43ccbbabd4687
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2022-12-24 08:18:19 -0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,98 +1,34 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect, useState } from 'react';
+import "react-toastify/dist/ReactToastify.css";
+import './App.css';
+
+import {  useState, useEffect } from 'react';
+import { ethers } from "ethers";
+import {ToastContainer, toast} from "react-toastify";
+
 import WRHeader from 'wrcomponents/dist/WRHeader';
-import WRFooter from 'wrcomponents/dist/WRFooter';
+import WRFooter, { async } from 'wrcomponents/dist/WRFooter';
 import WRInfo from 'wrcomponents/dist/WRInfo';
 import WRContent from 'wrcomponents/dist/WRContent';
 import WRTools from 'wrcomponents/dist/WRTools';
-import { ethers } from "ethers";
-import './App.css';
-import {ToastContainer, toast} from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import Button from "react-bootstrap/Button";
+
+import { format6FirstsAnd6LastsChar } from "./utils";
+import meta from "./assets/metamask.png";
 
 function App() {
 
-  function toastMessage(text) {
-    toast.info(text)  ;
-  }
-  const [connected, setConnected] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
+  const [provider, setProvider] = useState();
+  const [signer, setSigner] = useState();
+  
   const [name, setName] = useState('');
   const [names, setNames] = useState([]);
 
-  //contract goerli testnet
-  //const addressContract = ''
-  //contract Ganache
-  const addressContract = '0x7136c0cA69f243d146b31B741445c619054b371e'
+  const contractAddress = '0x4DAe7F772Ef5C1824736A7A069C0239582eD6527'
 
   const abi = [
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "name": "names",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "position",
-          "type": "uint256"
-        }
-      ],
-      "name": "get",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
-    {
-      "inputs": [],
-      "name": "getAll",
-      "outputs": [
-        {
-          "internalType": "string[]",
-          "name": "",
-          "type": "string[]"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
-    {
-      "inputs": [],
-      "name": "getLength",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function",
-      "constant": true
-    },
     {
       "inputs": [
         {
@@ -112,86 +48,234 @@ function App() {
       "outputs": [],
       "stateMutability": "nonpayable",
       "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "position",
+          "type": "uint256"
+        }
+      ],
+      "name": "get",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getAll",
+      "outputs": [
+        {
+          "internalType": "string[]",
+          "name": "",
+          "type": "string[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getLength",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "names",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
     }
   ];
 
-  let contractDeployed = null;
-  let contractDeployedSigner = null;
-  
-  function getProvider(){
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    if (contractDeployed == null){
-      contractDeployed = new ethers.Contract(addressContract, abi, provider)
+  async function handleConnectWallet (){
+    try {
+      setLoading(true)
+      let prov =  new ethers.providers.Web3Provider(window.ethereum);
+      setProvider(prov);
+
+      let userAcc = await prov.send('eth_requestAccounts', []);
+      setUser({account: userAcc[0], connected: true});
+
+      const contrSig = new ethers.Contract(contractAddress, abi, prov.getSigner())
+      setSigner( contrSig)
+
+    } catch (error) {
+      toastMessage(error.reason)
+    } finally{
+      setLoading(false);
     }
-    if (contractDeployedSigner == null){
-      contractDeployedSigner = new ethers.Contract(addressContract, abi, provider.getSigner());
-    }
-  }
-
-  async function connectMetaMask (){
-    if(typeof window.ethereum !== "undefined"){
-        try
-        {
-            await window.ethereum.request({ method: "eth_requestAccounts" });
-            setConnected(true)
-            toastMessage("You are connected");
-        }
-        catch (error) {
-            console.log(error);
-            setConnected(false);
-            toastMessage("Oops.. An error, sorry..");
-        }
-    }
-    else {
-        setConnected(false)
-        toastMessage("Conect your metamask.")
-      }
-  }
-  
-  function handleName(e) {
-    setName(e.target.value);
-  }
-
-  async function getNames(){
-    getProvider();
-    const namesLoaded = await contractDeployed.getAll();
-    console.log(namesLoaded);
-    setNames (namesLoaded);
-    toastMessage (`Names loaded`);
-  }
-
-  async function addName(name) {
-    getProvider();
-    const resp = await contractDeployedSigner.addName(name);
-    setNames([...names, name]);
-    toastMessage('Name added');
-  }
-
-  async function removeLast(){
-    getProvider();
-    await contractDeployedSigner.removeLast();
-    toastMessage('Last name removed, wait some seconds and reload');
   }
 
   useEffect(() => {
     
-    //getProvider();
+    async function getData() {
+      try {
+        const {ethereum} = window;
+        if (!ethereum){
+          toastMessage('Metamask not detected');
+        }
   
-    return () => {
-    
+        const goerliChainId = "0x5";
+        const currentChainId = await window.ethereum.request({method: 'eth_chainId'})
+        if (goerliChainId != currentChainId){
+          toastMessage('Change to goerli testnet')
+        }    
+      } catch (error) {
+        toastMessage(error.reason)        
+      }
+      
     }
+
+    getData()  
+    
   }, [])
+
+  function toastMessage(text) {
+    toast.info(text)  ;
+  }
+
+  async function isConnected(){
+    if (!user.connected){
+      toastMessage('You are not connected!')
+      return false;
+    }
+    return true;
+  }
+
+  async function handleDisconnect(){
+    try {
+      setUser({});
+      setSigner(null);
+      setProvider(null);
+    } catch (error) {
+      toastMessage(error.reason)
+    }
+  }
   
+  async function getNames(){
+
+    try {
+      if (!isConnected()) {
+        return;
+      }
+      const namesLoaded = await signer.getAll();
+      setNames (namesLoaded);
+      toastMessage (`Names loaded`);
+    } catch (error) {
+      console.log(error);
+      toastMessage(error.reason)      
+    } finally{
+      setLoading(false);
+    }
+
+  }
+
+  async function getLength(){
+
+    try {
+      if (!isConnected()) {
+        return;
+      }
+      const length = await signer.getLength();
+      toastMessage (`The length is ${length}`);
+    } catch (error) {
+      console.log(error);
+      toastMessage(error.reason)      
+    } finally{
+      setLoading(false);
+    }
+
+  }
+
+  async function addName(name) {
+    
+    try {
+      if (!isConnected()) {
+        return;
+      }
+      setLoading(true)
+      const resp = await signer.addName(name);
+      await resp.wait();
+      setNames([...names, name]);
+      toastMessage('Name added');
+    } catch (error) {
+      toastMessage(error.reason)      
+    } finally{
+      setLoading(false);
+    }
+
+  }
+
+  async function removeLast(){
+    
+    try {
+
+      if (!isConnected()) {
+        return;
+      }
+      setLoading(true)
+      const resp = await signer.removeLast();
+      await resp.wait();
+      toastMessage('Last name removed');
+    } catch (error) {
+      toastMessage(error.reason)      
+    } finally{
+      setLoading(false);
+    }
+
+    
+  }
 
   return (
     <div className="App">
       <ToastContainer position="top-center" autoClose={5000}/>
       <WRHeader title="Advanced storage smart contract" image={true} />
-      <WRInfo chain="Goerli testnet" />
+      <WRInfo chain="Goerli" testnet={true} />
       <WRContent>
-        <button onClick={connectMetaMask}>Connect your wallet</button>
+        
+        {loading && 
+          <h1>Loading....</h1>
+        }
+        { !user.connected ?<>
+            <Button className="commands" variant="btn btn-primary" onClick={handleConnectWallet}>
+              <img src={meta} alt="metamask" width="30px" height="30px"/>Connect to Metamask
+            </Button></>
+          : <>
+            <label>Welcome {format6FirstsAnd6LastsChar(user.account)}</label>
+            <button className="btn btn-primary commands" onClick={handleDisconnect}>Disconnect</button>
+          </>
+        }
         <hr/>
-        <button onClick={getNames}>Get all names</button>
+        
+        <button className="btn btn-primary commands" onClick={getNames}>Get all names</button>
         <div>
           <ul>
             {names.map((nameArray, i) => 
@@ -199,11 +283,13 @@ function App() {
             )}
           </ul>
         </div>
-        <button onClick={removeLast}>Remove last name</button>
+        <button className="btn btn-primary commands" onClick={getLength}>Length</button>
+        <hr/>
+        <button className="btn btn-primary commands" onClick={removeLast}>Remove last name</button>
         <hr/>
         <span>Type your name to save in blockchain</span>
-        <input type="text" onChange={handleName} value={name}/>
-        <button onClick={() => addName(name)}>Add name</button>
+        <input className="commands" type="text" onChange={(e) => setName(e.target.value)} value={name}/>
+        <button className="btn btn-primary commands" onClick={() => addName(name)}>Add name</button>
         
 
       </WRContent>
